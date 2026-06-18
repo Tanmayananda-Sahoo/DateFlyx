da0ebd8ead726a4b8e465786688cbfffbe247229
Tanmayananda Sahoo <boyvector06@gmail.com>
2026-06-18 23:04:38 +0530

    routing import fixing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage.jsx';
 import CafeListPage from './pages/CafeListPage.jsx';
 import AboutUsPage from './pages/AboutUsPage.jsx';
